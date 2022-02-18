4c9b91c96d19f94276dcc6aa1efe5e84849cddbf
Chitus <alejandropj1996@gmail.com>
2022-02-18 12:42:16 +0100

    Creacion

---
+++ src/main/java/AutoescuelasAlpine/InicioAlpineApplication.java
@@ -0,0 +1,13 @@
+package AutoescuelasAlpine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InicioAlpineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InicioAlpineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AutoescuelasAlpine/InicioAlpineApplicationTests.java
@@ -0,0 +1,13 @@
+package AutoescuelasAlpine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InicioAlpineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
