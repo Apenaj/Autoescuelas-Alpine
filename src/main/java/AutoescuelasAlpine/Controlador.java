65ee36e3f35f0d18869bfe28b212927adbd58ef2
ja.conde <ja.conde@VDDES192.dcloud.local>
2022-02-18 18:35:58 +0100

    Commit de prueba

---
+++ src/main/java/AutoescuelasAlpine/Controlador.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package AutoescuelasAlpine;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author ja.conde
+ *
+ */
+@Controller
+public class Controlador {
+	
+	@GetMapping("/bienvenida")
+	public String bienvenida(Model model) {
+		model.addAttribute("name", "Autoescuelas Alpine");
+		
+		return "Pagina_bienvenida";
+	}
+
+}
