4917e08b9b438c318349fce6c978db7b85d90301
ja.conde <ja.conde@VDDES031.dcloud.local>
2022-03-14 18:07:22 +0100

    Añadido soporte de csrf

--- src/main/java/AutoescuelasAlpine/seguridad/SecurityConfiguration.java
+++ src/main/java/AutoescuelasAlpine/seguridad/SecurityConfiguration.java
@@ -52,7 +52,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		http.logout().logoutSuccessUrl("/");
 
 		// Disable CSRF at the moment
-		http.csrf().disable();
+		//http.csrf().disable();
 	}
 
 }
